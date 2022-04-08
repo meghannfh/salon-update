d5f3d2f5fa462553fa135f12e1ce25bbc41fea58
meghann <meghannfh@gmail.com>
2022-04-08 18:46:58 +0900

    Salon first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+        navToggle.classList.add('hide');
+        navClose.classList.remove('hide');
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+        navToggle.classList.remove('hide');
+        navClose.classList.add('hide');
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+    navToggle.classList.remove('hide')
+    navClose.classList.add('hide')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
